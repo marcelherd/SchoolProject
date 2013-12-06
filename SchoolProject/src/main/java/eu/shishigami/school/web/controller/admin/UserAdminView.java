1c772d9ca08a983743675b7085369acd2df1f9d9
HerdMA <marcel-andre.herd@basf.com>
2013-12-06 11:10:07 +0100

    Added admin pages

---
+++ SchoolProject/src/main/java/eu/shishigami/school/web/controller/admin/UserAdminView.java
@@ -0,0 +1,21 @@
+package eu.shishigami.school.web.controller.admin;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import eu.shishigami.school.domain.UserEntity;
+
+@Component
+@Scope(value = "session")
+@Getter
+@Setter
+public class UserAdminView {
+	
+	private List<UserEntity> allUsers;
+
+}
