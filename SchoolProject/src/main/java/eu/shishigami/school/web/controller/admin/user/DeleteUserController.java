5ddffd6b0116d369e597a201f342dccbeb974644
Marcel Herd <info@shishigami.eu>
2013-12-12 19:22:28 +0100

    Added custom dialog for delete user

--- SchoolProject/src/main/java/eu/shishigami/school/web/controller/admin/user/DeleteUserController.java
+++ SchoolProject/src/main/java/eu/shishigami/school/web/controller/admin/user/DeleteUserController.java
@@ -1,7 +1,10 @@
 package eu.shishigami.school.web.controller.admin.user;
 
+import javax.annotation.PostConstruct;
+
 import lombok.extern.slf4j.Slf4j;
 
+import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -14,12 +17,25 @@ import eu.shishigami.school.util.LoggingUtil;
 @Scope(value = "session")
 @Slf4j
 public class DeleteUserController {
+	
+	@Autowired
+	private DeleteUserView deleteUserView;
 
 	@Autowired
 	private UserService userService;
 	
-	public void handleDeleteUser(UserEntity userEntity) {
-		userService.delete(userEntity);
+	@PostConstruct
+	public void init() {
+		LoggingUtil.logInitialization(log);
+	}
+	
+	public void handleSelectUser(UserEntity userEntity) {
+		deleteUserView.setUserEntity(userEntity);
+		init();
+	}
+	
+	public void handleDeleteUser() {
+		userService.delete(deleteUserView.getUserEntity());
 		LoggingUtil.logDelete(log);
 	}
 	
---
+++ SchoolProject/src/main/java/eu/shishigami/school/web/controller/admin/user/DeleteUserView.java
@@ -0,0 +1,19 @@
+package eu.shishigami.school.web.controller.admin.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import eu.shishigami.school.domain.UserEntity;
+
+@Component
+@Scope(value = "session")
+@Getter
+@Setter
+public class DeleteUserView {
+
+	private UserEntity userEntity;
+	
+}
